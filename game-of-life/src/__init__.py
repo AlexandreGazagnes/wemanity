d2d3f567f41a772b2d6c98b87c1386112ec49e64
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-09 16:46:22 +0200

    update global consts for game

---
+++ game-of-life/src/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#-*- coding: utf8 -*-
+
+
+"""
+__init__.py
+"""
+
+
+# import 
+
+
+# constants 
+
+SIZE 		= None 		# space dim
+INIT_STATE	= None		# number of random cells
+
+MAX_LOOP 	= None		# ask to jump /stop an x number of loop
+
+AUTO 		= False		# auto or handly new loop
+WAITER 		= 1 		# second
+
+
+
