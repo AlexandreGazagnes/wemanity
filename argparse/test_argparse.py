e58db7562f0cd28e09117405b16a1b547781ff97
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-09 09:37:55 +0200

    TDD : writting canevas for test functions

--- argparse/test_argparse.py
+++ argparse/test_argparse.py
@@ -11,3 +11,34 @@ tests
 
 import pytest, unittest, os
 from argparse import * 
+
+
+"""
+please use "pytest -v" rather than unittest
+"""
+
+
+# functions
+
+def test_do_not_raise_error() :
+	"""test program don not fail in normal conditions"""
+	
+	pass
+
+
+def test_do_raise_error() : 
+	""" test good exception are raised"""
+
+	pass
+
+
+def test_detect_args() : 
+	"""test good return of argparse function"""
+
+	pass
+
+
+# main	
+
+if __name__ == '__main__':
+	main()
