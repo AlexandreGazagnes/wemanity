65a55b9815a71c64f02f292b52fe0bed9e1cdc4b
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-09 09:39:37 +0200

    writting argparse.py headers

---
+++ argparse/argparse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#-*- coding: utf8 -*-
+
+
+"""
+argparse.py
+"""
+
+
+
+# Find here argparse.py, wich is a short script to handle and manage CLI
+# args when a program is launched. 
+
+# This script is was written without importing argparse - seems to be too easy 
+# for this question
+
+
+# import
+
+import sys, logging, os	
+
+
+# logging
+
+l = logging.INFO
+logging.basicConfig(level=l, format='%(levelname)s : %(message)s')
