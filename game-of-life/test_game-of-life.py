76df980ac5b4e99c450a76489465680849812c48
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-09 16:41:29 +0200

    update all test functions

--- game-of-life/test_game-of-life.py
+++ game-of-life/test_game-of-life.py
@@ -7,9 +7,110 @@ test_game-of-life.py
 """
 
 
-
+"""
+please use 'pytest -v'
+"""
 
 
 # import 
 
-import os, sys, logging
+import os, sys, logging, pytest
+
+
+# functions
+
+def test_intro() : 
+	"""test intro"""
+
+	pass
+
+
+def test_close() : 
+	"""test close """
+
+	pass
+
+
+def test_arg_manager0() : 
+	"""test arg manager without any cli"""
+
+	# without any cli
+
+	pass
+
+
+def test_arg_manager1() : 
+	"""test arg manager with one or 2 max cli"""
+
+	# with one or 2 max cli
+
+	pass
+
+
+def test_arg_manager1() : 
+	"""test arg manager with all cli avialable """
+
+	# with all cli avialable 
+
+	pass
+
+
+def test_GameOfLife0() : 
+	"""just init a GameOfLife instance"""
+
+	# just init a GameOfLife with default features
+
+	pass
+
+
+def test_GameOfLife1() : 
+	"""just init a GameOfLife instance"""
+
+	# just init a GameOfLife with random features
+	
+	pass
+
+
+def test_GameOfLife2() : 
+	"""just init a GameOfLife instance"""
+
+	# just init a GameOfLife with random features
+	
+	pass
+
+
+def test_expected_game0() : 
+	"""run a game session and assert result is conform to game expectation"""
+
+	# with just one cell
+	
+	pass
+
+
+def test_expected_game1() : 
+	"""run a game session and assert result is conform to game expectation"""
+
+	# with 2  cells
+
+	pass
+	
+
+
+def test_expected_game2() : 
+	"""run a game session and assert result is conform to game expectation"""
+
+	# with 3 cells
+	
+	pass
+
+
+def test_expected_game3() : 
+	"""run a game session and assert result is conform to game expectation"""
+
+	# with random state
+	
+	pass
+
+
+if __name__ == '__main__':
+	main()
