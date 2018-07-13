87034f89e9dea052e2f89c36fdd18afb2164ebd9
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-13 16:38:57 +0200

    written 90% of GameOfLife tests

--- game-of-life/tests/test_GameOfLife.py
+++ game-of-life/tests/test_GameOfLife.py
@@ -3,13 +3,14 @@
 
 
 """
-test_GameOfLfe.py
+test_GameOfLife.py
 """
 
 
 """
+test collection for GameOfLife.py
 please use 'pytest -v'
-"""   
+"""
 
 
 # import 
@@ -28,5 +29,62 @@ from src import *
 l = logging.INFO
 logging.basicConfig(level=l, format='%(levelname)s : %(message)s')
 
+
+# functions
+
+def test_init_GOL() : 
+	game = GameOfLife()
+
+
+def test_next_GOL() : 
+	game = GameOfLife()
+	game._next()
+
+
+def test_run_GOL() : 
+	game = GameOfLife()
+	iteration, exit_value = game.run()
+	assert isinstance(iteration, int)
+	assert isinstance(exit_value, int)
+
+
+def test_GOL_block() :
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run() 
+
+
+def test_GOL_Beehive() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
+
+
+def test_GOL_Loaf() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
+
+
+def test_GOL_Boat() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
+
+
+def test_GOL_Tub() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
+
+
+def test_GOL_Blinker() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
+
+
+def test_GOL_Toad() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
+
+
+def test_GOL_Beacon() : 
+	game = GameOfLife(init_cells:[(0,0), (0,0), ...])
+	assert (iteration, exit_value) == game.run()
 
 
