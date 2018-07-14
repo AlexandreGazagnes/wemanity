b25166bfa76294679911533985528271c2df93ed
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-14 12:57:45 +0200

    update conts with new default values

--- game-of-life/src/consts.py
+++ game-of-life/src/consts.py
@@ -17,23 +17,26 @@ see various comments bellow for more information
 # constants 
 
 # game dimension
-DIM_MIN 		= 5
-DIM_DEFAULT 		= 10
-DIM_MAX 		= 20
+DIM_DEFAULT 		= 20
+DIM_MIN 			= 5
+DIM_MAX 			= 30
 
 # if  INT --> # numb of cells in the space
-INIT_CELLS_DEFAULT 	= int(0.1 * DIM_DEFAULT**2)
+INIT_CELLS_DEFAULT 	= int(0.4 * DIM_DEFAULT**2)
 INIT_CELLS_MIN 		= 1
-INIT_CELLS_MAX 		= DIM_MAX
+INIT_CELLS_MAX 		= int(DIM_MAX*0.75)
 
 # if LIST of Tuples --> defaul location for cels
 # INIT_CELLS = [(3,2), (3,3) ...]
 
 # auto mode eg loop without user input 
-AUTO_DEFAULT 	= False
-WAITER_DEFAULT 	= 0.5
-WAITER_MIN		= 0.1
-WAITER_MAX 		= 1
+AUTO_MODE_DEFAULT	= True
+
+WAITER_DEFAULT 		= 0.2
+WAITER_MIN			= 0.1
+WAITER_MAX 			= 3
 
 # posible stop to avoid infinite loop
-MAX_ROUND_DEFAULT = 10
+MAX_ROUND_DEFAULT 	= 300
+MAX_ROUND_MIN 		= 10
+MAX_ROUND_MAX 		= 1000
