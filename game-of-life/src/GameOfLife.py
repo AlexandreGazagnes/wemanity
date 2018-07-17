6c57d98036cbd8995942ea59089f83432932c5c7
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-17 14:22:31 +0200

    rewrite as better as possible comments and docstrings

--- game-of-life/src/GameOfLife.py
+++ game-of-life/src/GameOfLife.py
@@ -8,16 +8,21 @@ GameOfLife.py
 
 
 """
-find here the main class of our project GameOfLife. Our argument manager (arg_manager) is called directly from the __init__ methode for good readability of game-of-life/main.py.
+find here the main class of our project GameOfLife. Our argument manager 
+(arg_manager) is called directly from the __init__ method for better readability
+of game-of-life/main.py.
+
 a specific effort regarding private args and method was done :)
-see all information bellow in doctrings and comments 
-enjoy :) :)
+
+to improve readability, lots of code refactoring options was not applied.
+
+see all information bellow in doctrings or ins comments 
 """
 
 
 # import 
 
-import os, sys, argparse, logging, random, time
+import os, sys, logging, random, time
 
 import pandas as pd
 import numpy as np
@@ -38,62 +43,72 @@ class GameOfLife(object) :
 	__CELL			= 	'o'
 
 	# exit status 
-	__EXIT_STATUS = { 		0 : "game is running / press <Enter> for new round",
-							1 : "all cells are dead, game frozen", 
-							2 : "max round reached, game ended",
-							3 : "game frozen, not evolution can appen (periodicty=1)",
-							4 : "game frozen, not evolution can appen (periodicty=2)"}
+	__EXIT_STATUS = { 	0 : "game is running / press <Enter> for new round",
+						1 : "all cells are dead, game frozen", 
+						2 : "max round reached, game ended",
+						3 : "game frozen, not evolution can appen (periodicty=1)",
+						4 : "game frozen, not evolution can appen (periodicty=2)"}
+
 
 	def __init__(self, options_dict=None) : 
-		""" init method 	
+		"""init method 	
 		
 		positional args  	: -  
-		optional args 		: options dict from arg_manager(), manualy written dict, or None
-		do 					: init an GameOfLife object and print out into / welcome msg	
-		return 				: a GameOfLife instance
+		optional args 		: option's dict (from arg_manager or manualy written)
+							  or None
+							  if option's dict is not 'None', arg_manager will
+							  automaticly check all data and ask user inputs
+							  if wrong data type or value  
+		do 					: init an GameOfLife object and print out 
+		                      into / welcome msg	
+		return 				: a GameOfLife object
 		raise				: - 
 		"""
 
 		# print intro
 		intro()
 
-		# if options -> check and ask, else arg_manager from scratch
-		options_dict = arg_manager(options_dict)
+		# if options -> check and ask, else ask arg_manager to manage user 
+		# interface and to build a valid options_dict from scratch
+		options_dict 				= arg_manager(options_dict)
 
-		# init attr from options
-		[ self.__setattr__(str("_"+key), val) for key, val in options_dict.items() ]
+		# init attr from options with an '_' before to control user acces to 
+		# specific attributes
+		[ self.__setattr__(str("_"+key), val) 
+										for key, val in options_dict.items() ]
 
 		# build default space fill with 0
 		i = np.arange(self.dim)
-		self._space  			= self.__build_default_space()
-		self.__default_space 	= self.__build_default_space()
+		self._space  				= self.__build_default_space()
+		self.__default_space 		= self.__build_default_space()
 
-		# round
+		# round counter for each iteration
 		self._round = 0
 
-		# list of all valid coord of the space
-		self.__list_of_coords = [	(i,j) 	for i in range(self._dim) 
-											for j in range(self._dim)]
+		# list of all valid coords of the space
+		self.__list_of_coords 		= [	(i,j) 	for i in range(self._dim) 
+												for j in range(self._dim)]
 
 		# if user just give a number => init a random location of cells 
 		if isinstance(self._init_cells, int) : 
+			self._cells 			= random.sample(	self.__list_of_coords, 
+														self._init_cells)
+			self._init_cells 		= self._cells
 
-			self._cells 		= random.sample(self.__list_of_coords, self._init_cells)
-			self._init_cells 	= self._cells
-
-		# else transform user cells in game's cells
+		# elif it is a list transform user cells in game's cells
 		elif isinstance(self._init_cells, list) : 
-
-			self._cells 		= self._init_cells
+			self._cells 			= self._init_cells
 
 		# record each cells of last round and last of last round
-		# to be able to recon 2 same states of the game 
-		self.__last_cells 		= self._cells
-		self.__2last_cells 		= self._cells
+		# to be able to recognize 2 same states of the game 
+		self.__last_cells 			= self._cells
+		self.__2last_cells 			= self._cells
 
 		# init game_state 
-		self._game_state 		= (self._round, self._cells, 0) 
+		self._game_state 			= (self._round, self._cells, 0) 
 
+
+	# properties : no comments no docstrings
 
 	@property
 	def round(self):
@@ -136,19 +151,20 @@ class GameOfLife(object) :
 
 	@property	
 	def space(self):
-		"""this is the UI game representation 
+		"""this is the UI game representation of the 'space' 
 		
 		positional args  	: -
 		optional args 		: -
 		do 					: -
-		return 				: return a specific str repr of game space
+		return 				: return a specific str to represtent the "game's 
+							  space"
 		raise				: - 
 		"""
 
 		txt = "\n"*2
 
 		# game representation / space
-		# use pandas.to string, but without index and columns name
+		# use pandas.to string, but without indexes and columns' name
 		s 	= self._space.to_string(index=False, header=False)
 		# replace 0 and 1 with readable values
 		s 	= s.replace("0", self.__WHITE_SPACE).replace("1", self.__CELL)
@@ -158,7 +174,6 @@ class GameOfLife(object) :
 		s 	= "\n|"+s
 		s 	= "---" * self.dim + s + "---" * self.dim +"\n"
 
-		# quit if needed
 		txt += 	s
 		txt += "\n"
 
@@ -173,6 +188,7 @@ class GameOfLife(object) :
 		txt += 	"init_cells  =  {}\n".format(self.init_cells_nb)
 		txt +=	"\n"*2
 
+		# quit if needed
 		txt +=	"press <Crlt + Z> to quit"
 		txt +=	"\n"*2
 		
@@ -180,7 +196,7 @@ class GameOfLife(object) :
 
 
 	def __str__(self) : 
-		"""jus overwrite teh __str__ method """
+		"""just overwrite the __str__ method """
 		
 		return self.space
 
@@ -190,39 +206,43 @@ class GameOfLife(object) :
 
 
 	def neighbours_nb(self, i, j):
-		"""for the cell at coord (i,j) give the num of living neighbours (min 0, max 8)"""
+		"""for the cell at coord (i,j) give the num of living neighbours 
+		(min 0, max 8)"""
 
-		return self.__count_neighbours(i,j)
+		return self.__neighbours_alive(i,j)
 
 
 	def neighbours_loc(self, i,j) : 
-		"""for the cell at coord (i,j) give the coords (k,l) of living neighbours"""
+		"""for the cell at coord (i,j) give the coords (k,l) of living 
+		neighbours"""
 
-		return self.__give_neighbours_coords(i,j)
+		return self.__neighbours_coords(i,j)
 
 
 	def __build_default_space(self) : 
-		"""private method : build a space of self.dim ** 2 cells, will all defualt values ie 0/dead
+		"""private method : build a space of self._dim ** 2 cells, will 
+		all defualt values ie 0/dead
 		
 		positional args  	: - 
 		optional args 		: -
 		do 					: -	
-		return 				: type pd.DataFrame : empty dataframe (full of 0) of shape self.dim * self.dim
+		return 				: type pd.DataFrame : empty dataframe (full of 0) 
+							  of shape self.dim * self.dim
 		raise				: - 
 		"""
 
-		i = np.arange(self.dim)
+		i = np.arange(self._dim)
 
 		return(pd.DataFrame(0, index=i , columns=i))
 
 
 	def __update_space(self) : 
-		"""method update incorporate cells from self._cells (list of coord of 
-		liviving cells in game space represenation
+		"""private method :  incorporate cells from self._cells (list of coords 
+		of living cells in game's space representation
 
 		positional args  	: - 
 		optional args 		: -
-		do 					: update self._space with self._cells loc as 1 	
+		do 					: update self._space with self._cells location as 1 	
 		return 				: - 
 		raise				: - 
 		"""
@@ -238,54 +258,57 @@ class GameOfLife(object) :
 		self._space = new_space
 
 
-	def __count_neighbours(self, i,j) : 
-		"""count how many living cells for one coord 
+	def __neighbours_alive(self, i,j) : 
+		"""private method : count how many living cells in the neighbourhood 
+		of one cell 
 
-		positional args  	: int i, int j wich stands for y, and x coord of a cell
+		positional args  	: int i, int j wich stands for y and x coords of 
+							  a specific cell
 		optional args 		: -
 		do 					: -	
-		return 				: type int :  number of living neighbours
+		return 				: an int as the number of living neighbours
 		raise				: - 
 		"""
 
-		# logging.info("__count_neighbours called")
-
 		neighbours = list()
 		
-		for (i,j) in self.__give_neighbours_coords(i,j) : 
+		for (i,j) in self.__neighbours_coords(i,j) : 
 			if self._space.iloc[i,j] == 1 : 
 				neighbours.append(1)
 		
 		return len(neighbours)
 
 
-	def __give_neighbours_coords(self, i,j)  : 
-		"""give all coords of direct neihbourhood for one cells
+	def __neighbours_coords(self, i,j)  : 
+		"""private method : give all possible coords of direct neihbourhood 
+		for one cell
 
-		positional args  	: int i, int j wich stands for y, and x coord of a cell
+		positional args  	: int i, int j wich stands for y and x coords of 
+							  a specific cell
 		optional args 		: -
 		do 					: -	
 		return 				: type list : list of tuples (y,x) as coords of existing neighbours
 		raise				: - 
 		"""
 
-		# logging.info("__give_neighbours_coords called")
-
 		candidates = [	(i+1, j+1), (i-1, j-1),
 						(i+1, j-1), (i-1, j+1),
 						(i, j-1), (i, j+1),
 						(i+1, j), (i-1, j)	]
 
-		autorized = [(i,j) for (i,j) in candidates if (i,j) in self.__list_of_coords]
+		li = [(i,j) for (i,j) in candidates if (i,j) in self.__list_of_coords]
 
-		return autorized
+		return li
 
 
 	def __update_cells(self) : 	
-		"""fmethod update to decide, regarding the neighbourhood, if a cell change its statse (living/dead) or 
+		"""private method : update for each cell, regarding the neighbourhood 
+		and game's rules, if a cell change of  state (living/dead) or 
 		stay alive/dead
-		rules : if alive with 2/3 neighbours alive : keep living, else is killed 
-		        if dead with 3 neighbours : wake up and live else keep deading
+
+		rules : if alive with 2/3 neighbours also alive : keep living, 
+				else is killed 
+		        if dead with 3 neighbours alive: wake up and live else stay dead
 
 		positional args  	: - 
 		optional args 		: -
@@ -294,14 +317,14 @@ class GameOfLife(object) :
 		raise				: - 
 		"""
 
-		# create 2 empty list of cells one to add and one to dell 
+		# create 2 empty list of cells, one to add and one to dell 
 		add_cells 	= list()
 		del_cells 	= list()
 		
 		# considering living cells 
 		for i,j in self.__list_of_coords :
 			# count how many neighbours  
-			neighbours = self.__count_neighbours(i,j)
+			neighbours = self.__neighbours_alive(i,j)
 
 			# if 2 or 3 neighbours --> stay alive 
 			if (i,j) in self._cells : 
@@ -324,23 +347,28 @@ class GameOfLife(object) :
 		new_cells = list(self._cells)
 
 		# from living cells : delete cells who die 
-		# use list comprehension
+		
+		# -------------------------------------
+		# -- > please use list comprehension
+		# -------------------------------------
+
 		for (i,j) in self._cells : 
 			if (i,j) in del_cells : 
 				new_cells.remove((i,j))
 
-		# from list of nex living cells extend the curent list 
+		# with list of next living cells extend the curent list 
 		new_cells.extend(add_cells)
 
+		# update good attribute
 		self._cells = new_cells
 
 
 	def __detect_no_lives(self) : 
-		"""if cells == 0 stop the game
+		"""private method : if numb of living cells == 0, then  stop the game
 
 		positional args  	: -
 		optional args 		: -
-		do 					: -
+		do 					: print out specific message
 		return 				: True if no cells alive, else False 
 		raise				: - 
 		"""
@@ -348,28 +376,31 @@ class GameOfLife(object) :
 		if not self._cells  : 
 			end_no_lives()
 			return True
+		
 		return False
 
 
 	def __detect_last_round(self) : 
-		"""if max_round stop the game
+		"""private method : if max_round stop the game
 
 		positional args  	: -
 		optional args 		: -
-		do 					: -
-		return 				: True if game's iteration reached  self._max_round, 
-							  else False 
+		do 					: print out specific message
+		return 				: True if game's max iteration reached (ie 
+							  self._max_round) else False 
 		raise				: - 
 		"""
 
 		if self._round == self._max_round : 
 			end_last_round()
 			return True
+		
 		return False
 
 
 	def __detect_game_fixed(self, arg) : 
-		""" eval if game fixed with last_round
+		"""private method : eval if game fixed ie the game will not evol due to 
+		stablized structures (behive, block etc etc...)
 		
 		positional args  	: the cells list to compare to self._cells 
 		optional args 		: -
@@ -390,12 +421,13 @@ class GameOfLife(object) :
 
 
 	def __update_game_state(self) : 
-		"""regarding sef._cells, self.max_round, etc etc define if games stop or not 
+		"""private method : regarding self._cells, self.max_round, (etc etc) 
+		define if games has to stop or not 
 		
 		positional args  	: -
 		optional args 		: -
-		do 					: update self._game_state with self._round, self.cells_nb,
-							  and status (ie __EXIT_STATUS)
+		do 					: update self._game_state with self._round, 
+							  self.cells_nb,  and status (ie __EXIT_STATUS)
 		return 				: - 
 		raise				: -
 		"""
@@ -417,13 +449,15 @@ class GameOfLife(object) :
 
 
 	def __looper(self) : 
-		"""find here the looper method wich is used to deal with auto_mode True/Flase
+		"""private method : find here the 'looper' method which is used to 
+		deal with auto_mode True/False
+
 		record last_cells and 2_last_cells to track fixed structures
-		and increment round
+		and increment self._round
 		
 		positional args  	: -
 		optional args 		: -
-		do 					: handle user interface for next iteration, 
+		do 					: handle user interface for next iteration if needed,
 							  update self._round, self.__last_cells and 
 							  self.__2last_cells,  
 		return 				: - 
@@ -436,27 +470,29 @@ class GameOfLife(object) :
 
 		# else ask to user input before continuing
 		else : 
-			input( "press <Enter> for next iteration"
+			input( 	"press <Enter> for next iteration\n"
 					"press <Crlt + Z> to quit \n")
 
 		# incremenent round
 		self._round+=1
-		# record last list of cells for dectecting fixed game (periodicity =1)
+
+		# record last list of cells for dectecting fixed game (periodicity of 1)
 		self.__last_cells = self._cells
-		# record lastof last  list of cells for dectecting fixed game (periodicity =2)		
+		
+		# record lastof last  list of cells for dectecting fixed game (periodicity of 2)		
 		if self._round %2 : 
 			self.__2last_cells = self._cells
 
 
 	def _start(self) : 
-		"""first game representation for user, ask 
+		"""first game representation for user 
 
 		positional args  	: -
 		optional args 		: -
-		do 					: update self._space and print it, ask user input to start 
+		do 					: update self._space and print it, ask user input 
+							  to start 
 		return 				: - 
 		raise				: - 
-
 		"""
 
 		os.system("clear")
@@ -467,12 +503,12 @@ class GameOfLife(object) :
 		print(self.space)
 		
 		# user input 
-		print("press <Enter> to start")
+		print("press <Enter>    to start")
 		input("press <Crlt + Z> to quit\n")	
 
 
 	def _next(self) :
-		""" update game's state and show game repr for each round
+		"""update game's state and show game repr for each round
 
 		positional args  	: -
 		optional args 		: -
@@ -486,10 +522,14 @@ class GameOfLife(object) :
 		os.system("clear")
 		game()
 
+		# update _cells and _space
 		self.__update_cells()
 		self.__update_space()
+		
+		# print game repr
 		print(self.space)
 
+		# update _game_state and call __looper if needed
 		self.__update_game_state()
 		# if exit status == 0 call __looper() 
 		if not self._game_state[2] : 				
@@ -523,8 +563,6 @@ class GameOfLife(object) :
 			if self._game_state[2] : 
 				# if scenario occuring return 3 dim tupple
 				return self._game_state
-
-
 
 
 
