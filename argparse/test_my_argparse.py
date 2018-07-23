ab2fb816dd1a4158f65c5113b08743a0c060ccee
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-23 11:04:42 +0200

    convert indetation to space

--- argparse/test_my_argparse.py
+++ argparse/test_my_argparse.py
@@ -21,54 +21,54 @@ from my_argparse import my_parser
 # functions
 
 def test_do_not_raise_error() :
-	"""test program do not fail in normal conditions"""
+    """test program do not fail in normal conditions"""
 
-	os.system("python3 my_argparse.py")
-	os.system("python3 my_argparse.py -l ")
-	os.system("python3 my_argparse.py -p 8080 ")
-	os.system("python3 my_argparse.py -d /home/")
-	os.system("python3 my_argparse.py -l -p 8080 -d /home/")
-	os.system("python3 my_argparse.py -l -p 8080 -k 12222 -f 'sdnie' -d /home/")
+    os.system("python3 my_argparse.py")
+    os.system("python3 my_argparse.py -l ")
+    os.system("python3 my_argparse.py -p 8080 ")
+    os.system("python3 my_argparse.py -d /home/")
+    os.system("python3 my_argparse.py -l -p 8080 -d /home/")
+    os.system("python3 my_argparse.py -l -p 8080 -k 12222 -f 'sdnie' -d /home/")
 
 
 def test_do_raise_error() : 
-	""" test good exception are raised"""
+    """ test good exception are raised"""
 
-	with pytest.raises(TypeError) : 
-		my_parser(1226789)
+    with pytest.raises(TypeError) : 
+        my_parser(1226789)
 
-	with pytest.raises(TypeError) : 
-		my_parser("python3 my_argparse.py -p 80aa80 ")
+    with pytest.raises(TypeError) : 
+        my_parser("python3 my_argparse.py -p 80aa80 ")
 
-	with pytest.raises(NotADirectoryError) : 		
-		my_parser("python3 my_argparse.py -d -usr-logs")
+    with pytest.raises(NotADirectoryError) :        
+        my_parser("python3 my_argparse.py -d -usr-logs")
 
 
 def test_detect_args0() : 
-	"""test good return of argparse function"""
+    """test good return of argparse function"""
 
-	args = my_parser("python3 my_argparse.py -l -p 8080 -d /home/")
-	assert len(args) == 3
-	assert isinstance(args, dict)	
+    args = my_parser("python3 my_argparse.py -l -p 8080 -d /home/")
+    assert len(args) == 3
+    assert isinstance(args, dict)   
 
 
 def test_detect_args1() : 
-	"""test good return of argparse function"""
+    """test good return of argparse function"""
 
-	args = my_parser("python3 my_argparse.py")
-	assert len(args) == 3
-	assert isinstance(args, dict)	
+    args = my_parser("python3 my_argparse.py")
+    assert len(args) == 3
+    assert isinstance(args, dict)   
 
 
 def test_detect_args2() : 
-	"""test good return of argparse function"""
+    """test good return of argparse function"""
 
-	args = my_parser("python3 my_argparse.py -l -p 8080 -d /home/ -k False -v 122 ")
-	assert len(args) == 3
-	assert isinstance(args, dict)	
+    args = my_parser("python3 my_argparse.py -l -p 8080 -d /home/ -k False -v 122 ")
+    assert len(args) == 3
+    assert isinstance(args, dict)   
 
 
-# main	
+# main  
 
 if __name__ == '__main__':
-	main()
+    main()
